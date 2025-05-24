82422bbf0e83bfa21d023d89c877c336a270de8a
KellyJohannaRinconAgudelo Practica Git <kra0005@alu.medac.es>
2025-05-24 02:58:48 +0200

    Cambio conflictivo en secundaria2

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -3,7 +3,7 @@ class TestCuentaBancaria
 	public static void main(String args[])
 	{
 		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0);
-		CuentaBancaria cuentaNu = new CuentaBancaria("9876543210", "Otro Nombre", 500, 3);
+		CuentaBancaria cuentaNu = new CuentaBancaria("9999999999", "Cambio en secundaria", 500, 2);
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
 		cuenta.ingreso(100);
